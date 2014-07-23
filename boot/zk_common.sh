3bdbbacf7ed3d30830a362d8eae448eb5d16fd55
Matt Smillie <matt.smillie@joyent.com>
2014-07-22 19:29:16 -0700

    HEAD-1990 Create common submodule for shared zookeeper configs and setup.

--- boot/zk_common.sh
+++ boot/zk_common.sh
@@ -3,10 +3,25 @@
 #
 # Copyright (c) 2013 Joyent Inc., All rights reserved.
 #
+# Common steps to enable a zookeeper service in a zone.
 
-function setup_zk_delegated_dataset
+ZK_ROOT=/zookeeper
+
+function zk_common_import
 {
-    local ZK_ROOT=$1
+    # Install zookeeper package, need to touch this file to disable the license prompt
+    touch /opt/local/.dli_license_accepted
+
+    local SVC_ROOT=$1
+    local MANIFEST=${SVC_ROOT}/smf/manifests/zookeeper.xml
+
+    svccfg import ${MANIFEST} \
+        || fatal "unable to import Zookeeper from ${MANIFEST}"
+}
+
+# Sets up delegated dataset at /$ZK_ROOT/zookeeper
+function zk_common_delegated_dataset
+{
     local ZONE_UUID=$(zonename)
     local ZONE_DATASET=zones/$ZONE_UUID/data
     local mountpoint=
@@ -19,7 +34,22 @@ function setup_zk_delegated_dataset
 
     chmod 777 ${ZK_ROOT}
     sudo -u zookeeper mkdir -p ${ZK_ROOT}/zookeeper
+}
 
-    # accept the licence
-    touch /opt/local/.dli_license_accepted
+# sets up location of dataDir
+function zk_common_set_dataDir
+{
+    local MANIFEST_DIR=$1
+    local ZK_ROOT=$2
+
+    local IN_FILES=$(find ${MANIFEST_DIR} -name '*.in')
+    local FNAME=
+    local OUT_FILE=
+
+    for IN_FILE in IN_FILES; do
+        OUT_FILE=${IN_FILE%.in}
+        echo "Creating $(echo ${OUT_FILE} | cut -f6- -d'/')"
+        $(/opt/local/bin/gsed -e "s#@@ZK_ROOT@@#${ZK_ROOT}#" ${IN_FILE} \
+          > ${OUT_FILE})
+    done
 }
